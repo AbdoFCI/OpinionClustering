f72b3c81dfb95321e1fac14c3cf04a3e6e9cf2ed
Ali Ahmed <36967560+AliAhmedTaha@users.noreply.github.com>
2019-06-02 08:43:40 +0200

    formatting and commenting

--- Tag2vec/HashTag2Vec.py
+++ Tag2vec/HashTag2Vec.py
@@ -7,7 +7,7 @@ import numpy as np
 def sum_matrix(matrix):
     """
     sum the matrix to a vector
-    :param matrix:np.matrix
+    :param matrix: np.matrix
     :return: np.array
     """
     return np.sum(matrix,axis=0)
@@ -16,7 +16,7 @@ def sum_matrix(matrix):
 def avr_matrix(matrix):
     """
     avr the matrix to a vector
-    :param matrix:np.matrix
+    :param matrix: np.matrix
     :return: np.array
     """
     return np.average(matrix,axis=0)
@@ -31,8 +31,8 @@ class HashTag2Vic:
 
     def __init__(self,permutation=True,permutation_drop=0.5,method="sum"):
         """
-        :param permutation:
-        :param permutation_drop:
+        :param permutation: boolean
+        :param permutation_drop: float
         :param method: str sum => sum the vectors of tags avr => getting the avr of the tags
         """
         self.drop = permutation_drop
@@ -74,7 +74,6 @@ class HashTag2Vic:
     @staticmethod
     def _permutation(tags, cut_window=0,drop=0.5):
         """
-
         :param tags: list of list tags
         :param cut_window: int
         :param drop: float the parentage of the output
@@ -104,7 +103,6 @@ class HashTag2Vic:
 
     def op2vic(self,tags):
         """
-
         :param tags: list of tags that represent the opinion
         :return: numpy.array representations of opinion
         """
