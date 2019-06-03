335eb9e239e0b414ba6f7099d3a42a964a216872
Ali Ahmed <36967560+AliAhmedTaha@users.noreply.github.com>
2019-06-03 11:44:16 +0200

    Update mysqlManager.py

--- Data_manipulation/mysqlManager.py
+++ Data_manipulation/mysqlManager.py
@@ -3,8 +3,13 @@ from mysql.connector import Error, MySQLConnection
 from  Entities import Opinion
 
 def read_db_config(file_name='Data_manipulation\\config.ini', section='mysql'):
-    """Read database configuration file and return a dictionary object"""
-
+    """
+    Read database configuration file
+    :param file_name: str : dirictory of the configuration file
+    :param section: str : the name of mysql section
+    :return dictionary object
+    """
+    
     # create parser and read ini configuration file
     parser = ConfigParser()
     parser.read(file_name)
@@ -17,100 +22,14 @@ def read_db_config(file_name='Data_manipulation\\config.ini', section='mysql'):
             db[item[0]] = item[1]
     else:
         raise Exception('{0} not found in the {1} file'.format(section, file_name))
-
     return db
 
-
-# def query_with_fetchone():
-#     try:
-#         dbconfig = read_db_config()
-#         conn = MySQLConnection(**dbconfig)
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT * FROM books")
-#
-#         row = cursor.fetchone()
-#
-#         while row is not None:
-#             print(row)
-#             row = cursor.fetchone()
-#
-#     except Error as e:
-#         print(e)
-#
-#     finally:
-#         cursor.close()
-#         conn.close()
-#     return
-#
-#
-# def query_with_fetchall():
-#     rows = []
-#     try:
-#         dbconfig = read_db_config()
-#         conn = MySQLConnection(**dbconfig)
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT * FROM books")
-#         rows = cursor.fetchall()
-#         print('Total Row(s):', cursor.rowcount)
-#         for row in rows:
-#             print(row)
-#     except Error as e:
-#         print(e)
-#         return None
-#     finally:
-#         cursor.close()
-#         conn.close()
-#     return rows
-#
-#
-# def insert_book(title, isbn):
-#
-#     query = "INSERT INTO books(title,isbn) VALUES({},{})".format("car",1234)
-#     try:
-#         db_config = read_db_config()
-#         conn = MySQLConnection(**db_config)
-#         cursor = conn.cursor()
-#         cursor.execute(query)
-#
-#         conn.commit()
-#     except Error as error:
-#         print(error)
-#     finally:
-#         cursor.close()
-#         conn.close()
-#     return
-#
-#
-# def delete_book(book_id):
-#
-#     db_config = read_db_config()
-#
-#     query = "DELETE FROM books WHERE id = %s"
-#
-#     try:
-#         # connect to the database server
-#         conn = MySQLConnection(**db_config)
-#
-#         # execute the query
-#         cursor = conn.cursor()
-#         cursor.execute(query, (book_id,))
-#
-#         # accept the change
-#         conn.commit()
-#
-#     except Error as error:
-#         print(error)
-#
-#     finally:
-#         cursor.close()
-#         conn.close()
-#     return
-
-
-################################################################
-
-# Get opinions from the Database at some discussion and return them as a list of opinions
 def get_opinions(discussion_id):
+    """
+    Get opinions from the Database at some discussion
+    :param discussion_id: int
+    :return: list of opinions
+    """
     opinion_list = []
     opinion_sql_string = 'SELECT * FROM `opinion` WHERE `d_id` = ' +str(discussion_id)
     opinions = query_with_return(opinion_sql_string)
@@ -121,13 +40,23 @@ def get_opinions(discussion_id):
         op = Opinion.Opinion(id=opinion[0],text=opinion[1],hash_tags=tags)
         opinion_list.append(op)
     return opinion_list
-# return list of discussions
+
 def get_discussions():
+    """
+    Get the all discussions about some topic
+    :return: list of str
+    """
     discussion_sql_string = 'SELECT * FROM `descussion`'
     discussions_list = query_with_return(discussion_sql_string)
     return  discussions_list
-# Execute The sql quieries without results return back
+
+
 def query_without_results(sql_string):
+    """
+    Execute The sql quieries without results e.g. insert, delete, update
+    :param sql_string: str
+    :return: None
+    """
     conn = None
     cursor = None
     try:
@@ -135,7 +64,6 @@ def query_without_results(sql_string):
         conn = MySQLConnection(**db_config)
         cursor = conn.cursor()
         cursor.execute(sql_string)
-
         conn.commit()
     except Error as error:
         print(error)
@@ -144,8 +72,12 @@ def query_without_results(sql_string):
         conn.close()
     return
 
-# Execute The sql quieries with results return back
 def query_with_return(sql_string):
+    """
+    Execute The sql quieries with results e.g. select
+    :param sql_string: str
+    :return: resultset
+    """
     rows = None
     cursor = None
     conn = None
