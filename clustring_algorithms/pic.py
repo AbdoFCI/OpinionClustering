13409c2632b0eeb94b3db4d2fd6d4cc937a3bdb4
Abdelrahman Abdelaziz <abdelrahmanabdelaziz3@gmial.com>
2019-06-07 14:28:01 +0200

    Update pic.py
    send spark object instead of creating the connection inside the class , fixing errors , and send the model in the agreed shape

--- clustring_algorithms/pic.py
+++ clustring_algorithms/pic.py
@@ -1,48 +1,39 @@
 from pyspark.mllib.clustering import PowerIterationClustering, PowerIterationClusteringModel
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext
-from pyspark import sql
-from semantic_tree import Parser as pars
-import pandas as pd
-import numpy as np
-
 
 
 class PIC:
 
-    conf = SparkConf().setAppName("PySparkApp").setMaster("local[*]")
-    sc = SparkContext(conf=conf)
-    sqlContext = sql.SQLContext(sc)
-
-    def __init__(self,similarity_method):
+    def __init__(self, similarity_method, spark_object):
         """
 
         :param similarity_method: example tree_similarity , jaccard, edit distance
         """
 
         self.similarity_method = similarity_method
+        self.sparkContext = spark_object.getSparkContext()
+        self.sqlContext = spark_object.getSQLContext()
 
-    def _create_similarities_list(self, opinions, threshold = 0.5):
+    def _create_similarities_list(self, opinions, threshold=0.05):
         """
 
-        :param opinions:list of vector of string
-		:param threshold: double 
-        :return: similarities list : list of tupels 
+        :param opinions: list of vector of string
+        :param threshold: double
+        :return: similarities list : list of tuples
         """
         similarities_list = []
         outer_id = 0
         inner_id = 0
-        similarity = 0
         for outer in opinions:
             outer_id = outer_id + 1
             for inner in opinions:
                 inner_id = inner_id + 1
-                similarity = self.similarity_method(outer , inner)
-                if similarity >= threshold: similarities_list.append((outer_id, inner_id, similarity))
+                similarity = self.similarity_method(outer, inner)
+                if similarity >= threshold:
+                    similarities_list.append((outer_id, inner_id, similarity))
             inner_id = 0
         return similarities_list
 
-    def cluster(self,tags,n_clusters=4,threshold = 0.5, n_iterations = 25, initialization_mode = "degree"):
+    def cluster(self, tags, n_clusters=4, threshold=0.05, n_iterations=25, initialization_mode="degree"):
         """
 
         :param tags: list of vector of string
@@ -52,9 +43,11 @@ class PIC:
         :param initialization_mode: string ("degree", "random")
         :return: model
         """
-        similarities_list = self._create_similarities_list(tags, threshold = threshold)
-        rdd_data = self.sc.parallelize(similarities_list)
+        similarities_list = self._create_similarities_list(tags, threshold=threshold)
+        rdd_data = self.sparkContext.parallelize(similarities_list)
         model = PowerIterationClustering.\
-            train(similarities=rdd_data, n_clusters=n_clusters,
-                  n_iterations=n_iterations, initialization_mode=initialization_mode)
+            train(rdd_data, n_clusters,n_iterations, initialization_mode)
+        model_degr = model.assignments().collect()
+        model_degr.sort()
+        model = [assignment[1] for assignment in model_degr]
         return model
